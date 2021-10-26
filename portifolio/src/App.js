682f67a8d59a8269d541e8e52540be09009d81cf
Jonathan <knoppanc@gmail.com>
2021-10-26 16:40:56 -0400

    First commit

---
+++ portifolio/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+//element is the smallest block
+//components are many elements togtether
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Hello</h1>
+        <p>My name is jonathan im a soft dev</p>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ portifolio/src/index.js
@@ -0,0 +1,10 @@
+//default file
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//where we want react element to be inserted
+ReactDOM.render(<App></App>, document.getElementById('root'));
+
+
